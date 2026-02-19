51b0624f94f6c1b221d75086b9e3dcb8a554415b
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-02-19 16:58:16 +0300

    modified the syntax errors

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+    res.render("index", { user: req.user })
+}
+);
 app.get("/sign-up", (req, res) => res.render("sign-up-form"))
 
 app.post("/sign-up", async (req, res, next) => {
@@ -47,41 +50,43 @@ app.post(
 );
 
 
+
+
 passport.use(
-    new LocalStrategy(async (username, password, done) => {
-        try{
-            const { rows } = await pool.query("SELECT * FROM users WHERE username = 1$", [username]);
-            const user = rows[0];
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
 
-            if (!user) {
-                return done(null, false, {message: "Incorrect username"})
-            }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
 
-            if (user.password !== password) {
-                return done(null, false, { message: "Incorrect password"})
-            }
 
-            return done(null, user);
-        }catch(err){
-            return done(err)
-        }
-    })
-)
-
-passport.serializeUser( (user, done) => {
-   done(null, user.id);   
-})
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
 passport.deserializeUser(async (id, done) => {
-    try {
-        const { rows } = await pool.query("SELECT * FROM users WHERE id = 1$", [id]);
-        const user = rows[0];
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
 
-        done(null, user);
-    } catch {
-        done(err);
-    }
-})
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
 
 app.listen(3000, (error) => {
   if (error) {
