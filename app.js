23a07051d177de9f812e075bfa38664e71023db5
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-02-19 16:37:05 +0300

    created the passpport serializeer and deserializer

--- app.js
+++ app.js
@@ -59,6 +59,21 @@ passport.use(
     })
 )
 
+passport.serializeUser( (user, done) => {
+   done(null, user.id);   
+})
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const { rows } = await pool.query("SELECT * FROM users WHERE id = 1$", [id]);
+        const user = rows[0];
+
+        done(null, user);
+    } catch {
+        done(err);
+    }
+})
+
 app.listen(3000, (error) => {
   if (error) {
     throw error;
